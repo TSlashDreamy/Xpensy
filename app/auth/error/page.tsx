afa6b34f467be862270b14f049b1bee50f0f4daf
TSlash <tslashdreamy@gmail.com>
2026-07-13 00:00:22 +0300

    INIT: Configured vitest for unit testing. Added pre-push step for husky

--- app/auth/error/page.tsx
+++ app/auth/error/page.tsx
@@ -1,35 +1,18 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Suspense } from "react";
+import { ERROR_PAGE_TEST_ID } from "./constants";
 
-async function ErrorContent({
+export default async function Page({
   searchParams,
 }: {
-  searchParams: Promise<{ error: string }>;
+  searchParams: Promise<{ error?: string }>;
 }) {
   const params = await searchParams;
 
   return (
-    <>
-      {params?.error ? (
-        <p className="text-sm text-muted-foreground">
-          Code error: {params.error}
-        </p>
-      ) : (
-        <p className="text-sm text-muted-foreground">
-          An unspecified error occurred.
-        </p>
-      )}
-    </>
-  );
-}
-
-export default function Page({
-  searchParams,
-}: {
-  searchParams: Promise<{ error: string }>;
-}) {
-  return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    <div
+      className="flex min-h-svh w-full items-center justify-center p-6 md:p-10"
+      data-testid={ERROR_PAGE_TEST_ID}
+    >
       <div className="w-full max-w-sm">
         <div className="flex flex-col gap-6">
           <Card>
@@ -39,9 +22,15 @@ export default function Page({
               </CardTitle>
             </CardHeader>
             <CardContent>
-              <Suspense>
-                <ErrorContent searchParams={searchParams} />
-              </Suspense>
+              {params?.error ? (
+                <p className="text-sm text-muted-foreground">
+                  Code error: {params.error}
+                </p>
+              ) : (
+                <p className="text-sm text-muted-foreground">
+                  An unspecified error occurred.
+                </p>
+              )}
             </CardContent>
           </Card>
         </div>
